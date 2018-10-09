dd59fbe1fd0b252c3566927479d70d8dca594c2b
Kabir Goel <kabirgoel.kg@gmail.com>
2018-10-09 18:51:15 +0530

    Move components/Intro/Intro.js to components/Intro/index.js

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,3 +1,44 @@
-import Intro from './Intro'
+import React from 'react'
+import { Image } from 'react-native'
+import PageSwiper from './PageSwiper'
 
-export default Intro
+const logo = require('../../../assets/images/logo.png')
+const directions = require('./images/directions.png')
+
+const ExunLogo = (
+  <Image
+    style={{
+      width: 270,
+      height: 110,
+    }}
+    source={logo}
+    defaultSource={logo}
+    resizeMode="cover"
+  />
+)
+
+const Directions = (
+  <Image
+    style={{
+      width: 249,
+      height: 249,
+    }}
+    source={directions}
+    defaultSource={directions}
+    resizeMode="cover"
+  />
+)
+
+export default () =>
+  PageSwiper([
+    {
+      figure: ExunLogo,
+      heading: 'Welcome.',
+      body: 'Use the Exun 2018 app to get the latest updates about Exun 2018.',
+    },
+    {
+      figure: Directions,
+      heading: 'Notifications',
+      body: "You'll receive notifications when there's a new update, so you don't miss anything.",
+    },
+  ])
