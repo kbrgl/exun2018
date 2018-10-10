c90d6934b3331969c7b78102b279f3ac89bda4cd
Kabir Goel <kabirgoel.kg@gmail.com>
2018-10-10 19:57:12 +0530

    Remove artificial delay from saga

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,11 +1,9 @@
 import { put, takeLatest, call } from 'redux-saga/effects'
-import { delay } from 'redux-saga'
 import { POSTS_REFRESH, POSTS_REFRESH_SUCCESS } from '../constants'
 import api from '../api'
 
 function* fetchPosts() {
   const posts = yield call(api.fetchPosts)
-  yield call(delay, 400)
   yield put({ type: POSTS_REFRESH_SUCCESS, posts })
 }
 
