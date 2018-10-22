bd5e121c949f01edaae380601d156d7aca8011dc
Kabir Goel <kabirgoel.kg@gmail.com>
2018-10-22 23:26:07 +0530

    Add Sudocrypt easter egg

--- src/screens/Info.js
+++ src/screens/Info.js
@@ -1,12 +1,60 @@
 // @flow
 import React from 'react'
-import { StyleSheet, View, Image, ScrollView } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  Linking,
+  TouchableWithoutFeedback,
+  Alert,
+} from 'react-native'
 import Text from '../components/AppText'
 
 const unirely = require('../../assets/images/unirely.png')
 const athena = require('../../assets/images/athena.png')
 const digitalIndia = require('../../assets/images/digital-india.png')
 const logo = require('../../assets/images/logo.png')
+
+class SudocryptHeading extends React.Component {
+  constructor(props) {
+    super(props)
+    this.lastPress = 0
+    this.count = 0
+  }
+
+  onPress = () => {
+    const THRESHOLD = 2000
+    const now = new Date().getTime()
+    const delta = now - this.lastPress
+    if (delta < THRESHOLD) {
+      this.count += 1
+      if (this.count % 10 === 0) {
+        Alert.alert('This is the answer.')
+      }
+    } else this.count = 1
+    this.lastPress = now
+  }
+
+  render() {
+    return (
+      <TouchableWithoutFeedback onPress={this.onPress}>
+        <Text
+          style={{
+            marginLeft: 30,
+            marginTop: 30,
+            marginBottom: 30,
+            fontSize: 25,
+            fontWeight: 'bold',
+          }}
+        >
+          Sudocrypt
+        </Text>
+      </TouchableWithoutFeedback>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
@@ -30,6 +78,39 @@ export default () => (
         defaultSource={logo}
         resizeMode="cover"
       />
+      <SudocryptHeading />
+      <Text
+        style={{
+          marginLeft: 30,
+          marginRight: 30,
+        }}
+      >
+        Love the rush of solving puzzles? Relish the thrill of the chase? Adept at figuring out
+        enigmatic codes? Join us at Sudocrypt, an online cryptic hunt by Exun. Test your mettle
+        against the brightest minds in the country, crack devious tests of lateral thinking, claim
+        exciting prizes and the glory of victory!
+        {'\n\n'}
+      </Text>
+      <TouchableOpacity
+        onPress={() => {
+          const url = 'https://sudocrypt.com'
+          Linking.canOpenURL(url).then(supported => {
+            if (supported) {
+              Linking.openURL(url)
+            }
+          })
+        }}
+      >
+        <Text
+          style={{
+            marginHorizontal: 30,
+            marginBottom: 10,
+            color: '#2977f5',
+          }}
+        >
+          Play
+        </Text>
+      </TouchableOpacity>
       <Text
         style={{
           marginLeft: 30,
